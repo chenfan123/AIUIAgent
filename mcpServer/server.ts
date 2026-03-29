b350cf6a11dc644200b49e1ce3b85175fe44828e
陈健华 <chenjh@weipaitang.com>
2026-03-30 00:04:00 +0800

    mcp server

--- mcpServer/server.ts
+++ mcpServer/server.ts
@@ -1,5 +1,6 @@
 import { Server } from '@modelcontextprotocol/sdk/server/index.js';
 import { ListToolsRequestSchema } from '@modelcontextprotocol/sdk/types.js';
+import { StdioServerTransport } from '@modelcontextprotocol/sdk/server/stdio.js';
 
 const Tools = [
   {
@@ -22,8 +23,14 @@ export const createServer = () => {
       },
     },
   );
+  // 处理 ListToolsRequestSchema 请求，返回工具列表
   server.setRequestHandler(ListToolsRequestSchema, async () => ({
     tools: Tools,
   }));
+
+  const transport = new StdioServerTransport();
+
+  server.connect(transport);
+  console.error('MCP server running on stdio');
   return server;
 };
